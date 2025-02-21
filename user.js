09f62ac47c12d5c81d7de4c085ef361bd4759062
[yashgojariya21] <[yashgojariya07]>
2025-02-21 12:52:24 +0530

    add

--- user.js
+++ user.js
@@ -1,17 +1,16 @@
 const express = require("express");
 let app = express();
 const cors = require("cors");
-const corsConfig = {
-    origin: "*",
-    credential: true,
-    methods: ["GET", "POST"]
-};
-app.options("", cors(corsConfig))
+// const corsConfig = {
+//     origin: "*",
+//     credential: true,
+//     methods: ["GET", "POST"]
+// };
+// app.options("", cors(corsConfig))
 app.use(cors(corsConfig))
 require("./db/db")
 
 const PORT = process.env.PORT || 7000;
-
 
 const user_router = require("./routes/user")
 
